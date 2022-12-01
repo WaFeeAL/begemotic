847af21cf3b40b8e59968ae45b8f91a912113ebd
WaFeeAL <Herobrin504@gmail.com>
2022-12-01 23:43:09 +0700

    feat: Add ability to run application through python cli
    Signed-off-by: WaFeeAL <Herobrin504@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from os import environ
+
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host=environ.get('HOST', '127.0.0.1'),
+        port=environ.get('PORT', '8000'),
+        reload=True
+    )
