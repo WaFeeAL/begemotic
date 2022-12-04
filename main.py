dba16717ac70db5f43a281f09f158d54faeb2828
WaFeeAL <Herobrin504@gmail.com>
2022-12-05 00:52:47 +0700

    feat(begemotic): Connect router to application
    Signed-off-by: WaFeeAL <Herobrin504@gmail.com>

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 
+from apps.begemotic.routes import router
 from apps.core.exception_handler import override_exception_handler
 
 app = FastAPI()
 app.add_exception_handler(
     RequestValidationError, override_exception_handler
 )
+
+app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run(
